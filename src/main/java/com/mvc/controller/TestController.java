d24115f7a56f47be83cd5c0fddc1fed9235e0003
민형 <@DESKTOP-LTKCCK4>
2019-03-05 09:05:42 +0900

    d

--- src/main/java/com/mvc/controller/TestController.java
+++ src/main/java/com/mvc/controller/TestController.java
@@ -1,15 +1,14 @@
 package com.mvc.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
     
-	@RequestMapping(value="/hi", method=RequestMethod.GET)
+	@GetMapping("/hi")
 	public String hellow(){
-		return "hellow";
+		return "hellow22222";
 	}
 	
 }
