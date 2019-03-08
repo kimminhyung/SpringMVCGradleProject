740971ec1b84664ac9265616cbc6f8e50ae70b58
민형 <@DESKTOP-LTKCCK4>
2019-03-08 15:11:44 +0900

    boot strap 추가

--- src/main/java/com/mvc/controller/TestController.java
+++ src/main/java/com/mvc/controller/TestController.java
@@ -1,14 +1,16 @@
 package com.mvc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
     
-	@GetMapping("/hi")
-	public String hellow(){
-		return "hellow22222";
+	@GetMapping(path={"/hi/{userId}", "/smith/{userId}"} )
+//	public String hellow(@PathVariable int userId){
+	public String hellow(@PathVariable("useId") int userId) {
+		return "hellow22222 안녕하세요333 , userId{" + userId + "}";
 	}
 	
 }
